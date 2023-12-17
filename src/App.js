7467cb771025f93dcb431f687bd288e36d8eadd1
Seungwoo Yun <yoooonseungwoo@gmail.com>
2023-12-18 01:00:32 +0800

    chat done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import socket from './server'
 import InputField from './components/InputField/InputField.jsx'
+import MessageContainer from "./components/MessageContainer/MessageContainer.js"
 
 function App() {
   const [user, setUser] = useState(null)
@@ -36,6 +37,7 @@ function App() {
   return (
     <div>
       <div className="App">
+        <MessageContainer messageList={messageList} user={user} />
         <InputField message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
     </div>
